92d0b24d90087051f7f32ca1fd6dbbc566e51862
Christopher Miles <twitch@nervestaple.com>
2012-05-02 15:58:35 -0400

    Added rudimentary GZIP encoding.

--- src/com/tnrglobal/bishop/encoding.clj
+++ src/com/tnrglobal/bishop/encoding.clj
@@ -1,9 +1,45 @@
 ;;
 ;; Provides support for some default encoding methds
 ;;
-(ns com.tnrglobal.bishop.encoding)
+(ns com.tnrglobal.bishop.encoding
+  (:import [java.io PrintWriter InputStreamReader ByteArrayInputStream
+            ByteArrayOutputStream]
+           [java.util.zip GZIPInputStream GZIPOutputStream]))
 
 (defn identity-enc
   "Provides a pass-through identity encoding. The content is unchanged."
   [response]
   response)
+
+(defmulti gzip class)
+
+(defmethod gzip String [response]
+
+  ;; setup a byte array output stream wrapped around our gzip encoder
+  ;; and a writer
+  (with-open [byte-out (ByteArrayOutputStream.)
+              gzip-out (GZIPOutputStream. byte-out)
+              writer (PrintWriter. gzip-out)]
+
+    ;; write the data
+    (.write writer response)
+    (.flush writer)
+    (.finish gzip-out)
+
+    ;; return an input stream wrapping our output
+    (ByteArrayInputStream. (.toByteArray byte-out))))
+
+(defmethod gzip :default [response]
+  (gzip (str response)))
+
+(defn get-encoding-function
+  "Returns the appropriate encoding function for the provided String
+  encoding name."
+  [encoding]
+  (cond
+
+    (= "identity" encoding)
+    identity-enc
+
+    (= "gzip" encoding)
+    gzip))
