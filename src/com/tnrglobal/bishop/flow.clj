01fe9a98dde39e74288ece7ff505f9f2fd178932
Joel Boehland <joel.boehland@evocomputing.com>
2012-05-12 15:08:44 -0700

    Added docs to FSM state functions

--- src/com/tnrglobal/bishop/flow.clj
+++ src/com/tnrglobal/bishop/flow.clj
@@ -163,19 +163,23 @@
     #(o18b resource request response (assoc state :o18 false))))
 
 (defn o20
-  [resource request response state]
+  "Test if response includes an entity"
+  [resource request response state]  
   (if (nil? (:body response))
     (response-code 204 request response state :o20)
     #(o18 resource request response (assoc state :o20 false))))
 
 (defn p11
-  [resource request response state]
+  "Test if this is a new resource"
+  [resource request response state]  
   (if (not (some #(= "Location" %) (keys (:headers response))))
     #(o20 resource request response (assoc state :p11 true))
     (response-code 201 request response state :p11)))
 
 (defn p3
+  "Test if there is a conflict"
   [resource request response state]
+  
   (if (apply-callback request resource :is-conflict?)
     (response-code 409 request response state :p3)
     #(p11 resource
@@ -184,6 +188,7 @@
           (assoc state :p3 false))))
 
 (defn o14
+  "Test if there is a conflict"
   [resource request response state]
   (if (apply-callback request resource :is-conflict?)
     (response-code 409 request response state :o14)
@@ -193,12 +198,14 @@
           (assoc state :o14 false))))
 
 (defn o16
+  "Test if this is an HTTP PUT request"
   [resource request response state]
   (if (= :put (:request-method request))
     #(o14 resource request response (assoc state :o16 true))
     #(o18 resource request response (assoc state :o16 false))))
 
 (defn n11
+  "Test if this is a redirect"
   [resource request response state]
   (let [create (apply-callback request resource :post-is-create?)]
     (cond
@@ -253,12 +260,14 @@
                 (assoc state :n11 false)))))))
 
 (defn n16
+  "Test if this is an HTTP POST request"
   [resource request response state]
   (if (= :post (:request-method request))
     #(n11 resource request response (assoc state :n16 true))
     #(o16 resource request response (assoc state :n16 false))))
 
 (defn n5
+  "Test if POST to missing resource is allowed"
   [resource request response state]
   (decide #(apply-callback request resource :allow-missing-post?)
           true
@@ -266,6 +275,7 @@
           (response-code 410 request response state :n5)))
 
 (defn m20b
+  "Test if the DELETE complete"
   [resource request response state]
   (let [delete-complete (apply-callback request resource :delete-completed?)]
     (if delete-complete
@@ -273,6 +283,7 @@
       (response-code 202 request response state :m20b))))
 
 (defn m20
+  "Test if the delete was successfully enacted immediately"
   [resource request response state]
   (let [delete-resource (apply-callback request resource :delete-resource)]
     (if delete-resource
@@ -280,12 +291,14 @@
       (response-code 500 request response state :m20))))
 
 (defn m16
+  "Test if this is an HTTP DELETE request"
   [resource request response state]
   (if (= :delete (:request-method request))
     #(m20 resource request response (assoc state :m16 true))
     #(n16 resource request response (assoc state :m16 false))))
 
 (defn m7
+  "Test if POST to missing resource is allowed"
   [resource request response state]
   (decide #(apply-callback request resource :allow-missing-post?)
           true
@@ -293,12 +306,14 @@
           (response-code 404 request response state :m7)))
 
 (defn m5
+  "Test if this is an HTTP POST request"
   [resource request response state]
   (if (= :post (:request-method request))
     #(n5 resource request response (assoc state :m5 true))
     (response-code 410 resource request response :m5)))
 
 (defn l17
+  "Test if Last-Modified is later than If-Modified-Since"
   [resource request response state]
   (let [last-modified (apply-callback request resource :last-modified)
         if-modified-since (:if-modified-since request)]
@@ -308,6 +323,7 @@
       (response-code 304 request response state :l17))))
 
 (defn l15
+  "Test if If-Modified-Since is later than now"
   [resource request response state]
   (if (> (.getTime (:if-modified-since request))
          (.getTime (Date.)))
@@ -315,6 +331,7 @@
     #(l17 resource request response (assoc state :l15 false))))
 
 (defn l14
+  "Test if If-Modified-Since is valid date"
   [resource request response state]
   (try
     (let [date (parse-header-date (header-value "if-modified-since"
@@ -327,18 +344,21 @@
       #(m16 resource request response (assoc state :l14 false)))))
 
 (defn l13
+  "Test if If-Modified-Since header exists"
   [resource request response state]
   (if (header-value "if-modified-since" (:headers request))
     #(l14 resource request response (assoc state :l13 true))
     #(m16 resource request response (assoc state :l13 false))))
 
 (defn l7
+  "Test if this is an HTTP POST request"
   [resource request response state]
   (if (= :post (:request-method request))
     #(m7 resource request response (assoc state :l7 true))
     (response-code 404 request response state :l7)))
 
 (defn l5
+  "Test if resource moved temporarily"
   [resource request response state]
   (let [moved-temp (apply-callback request resource :moved-temporarily?)]
     (if moved-temp
@@ -351,6 +371,7 @@
       #(m5 resource request response (assoc state :l5 false)))))
 
 (defn j18
+  "Test if this is an HTTP GET or HEAD request"
   [resource request response state]
   (if (or (= :get (:request-method request))
           (= :head (:request-method request)))
@@ -358,6 +379,7 @@
     (response-code 412 request response state :j18)))
 
 (defn k13
+  "Test if Etag is in If-None-Match header"
   [resource request response state]
   (let [if-none-match-etags
         (map make-unquoted
@@ -369,6 +391,7 @@
       #(l13 resource request response (assoc state :k13 false)))))
 
 (defn k5
+  "Test if resource Moved permanently"
   [resource request response state]
   (let [moved-permanently (apply-callback request resource :moved-permanently?)]
     (if moved-permanently
@@ -381,6 +404,7 @@
       #(l5 resource request response (assoc state :k5 false)))))
 
 (defn k7
+  "Test if resource previously existed"
   [resource request response state]
   (decide #(apply-callback request resource :previously-existed?)
           true
@@ -388,6 +412,7 @@
           #(l7 resource request response (assoc state :k7 false))))
 
 (defn i13
+  "Test if 'If-None-Match: *' header exists"
   [resource request response state]
   (let [if-none-match-value (header-value "if-none-match" (:headers request))]
     (if (and if-none-match-value (= "*" if-none-match-value))
@@ -395,12 +420,14 @@
       #(k13 resource request response (assoc state :i13 false)))))
 
 (defn i12
+  "Test if If-None-Match header exists"
   [resource request response state]
   (if (header-value "if-none-match" (:headers request))
     #(i13 resource request response (assoc state :i12 true))
     #(l13 resource request response (assoc state :i12 false))))
 
 (defn i4
+  "Test if resource moved permanently, then apply PUT to different URI"
   [resource request response state]
   (let [moved-perm (apply-callback request resource :moved-permanently?)]
     (if moved-perm
@@ -413,12 +440,14 @@
       #(p3 resource request response (assoc state :i4 false)))))
 
 (defn i7
+  "Test if this is an HTTP PUT request"
   [resource request response state]
   (if (= :put (:request-method request))
     #(i4 resource request response (assoc state :17 true))
     #(k7 resource request response (assoc state :i7 false))))
 
 (defn h12
+  "Test if Last-Modified is later than If-Unmodified-Since"
   [resource request response state]
   (let [last-modified (apply-callback request resource :last-modified)
         if-unmodified-since (parse-header-date
@@ -430,6 +459,7 @@
       #(i12 resource request response (assoc state :h12 false)))))
 
 (defn h11
+  "Test if If-Unmodified-Since is valid date"
   [resource request response state]
   (try
     (let [date (parse-header-date (header-value "if-unmodified-since" (:headers request)))]
@@ -441,12 +471,14 @@
       #(i12 resource request response (assoc state :h11 false)))))
 
 (defn h10
+  "Test if If-Unmodified-Since header exists"
   [resource request response state]
   (if (header-value "if-unmodified-since" (:headers request))
     #(h11 resource request response (assoc state :h10 true))
     #(i12 resource request response (assoc state :h10 false))))
 
 (defn h7
+  "Test if If-Match header exists"
   [resource request response state]
   (let [if-match-value (header-value "if-match" (:headers request))]
     (if (and if-match-value
@@ -455,6 +487,7 @@
       #(i7 resource request response (assoc state :h7 false)))))
 
 (defn g11
+  "Test if Etag in If-Match header"
   [resource request response state]
   (let [if-match-etags (map make-unquoted
                             (string/split (header-value "if-match"
@@ -466,6 +499,7 @@
       (response-code 412 request response state :g11))))
 
 (defn g9
+  "Test if 'If-Match *' header exists"
   [resource request response state]
   (let [if-match-value (header-value "if-match" (:headers request))]
     (if (and if-match-value
@@ -474,14 +508,15 @@
       #(g11 resource request response (assoc state :g9 false)))))
 
 (defn g8
+  "Test if If-Match header exists"
   [resource request response state]
   (if (header-value "if-match" (:headers request))
     #(g9 resource request response (assoc state :g8 true))
     #(h10 resource request response (assoc state :g8 false))))
 
 (defn g7
+  "Test if resource exists"
   [resource request response state]
-
   ;; compute our variances now that the headers have been handled, add
   ;; this to our response
   (let [vary (into (apply-callback request resource :variances)
@@ -496,6 +531,7 @@
             #(h7 resource request response-varied (assoc state :g7 false)))))
 
 (defn f7
+  "Test if acceptable encoding is available"
   [resource request response state]
   (let [headers (header-value "accept-encoding" (:headers request))
         acceptable (acceptable-encoding-type
@@ -509,12 +545,14 @@
            (assoc state :f7 true)))))
 
 (defn f6
+  "Test if Accept-Encoding header exists"
   [resource request response state]
   (if (header-value "accept-encoding" (:headers request))
     #(f7 resource request response (assoc state :f6 true))
     #(g7 resource request response (assoc state :f6 false))))
 
 (defn e6
+  "Test if acceptable charset is available"
   [resource request response state]
   (let [acceptable (acceptable-type
                     (let [charsets (apply-callback request resource
@@ -531,12 +569,14 @@
       (response-code 406 request response state :e6))))
 
 (defn e5
+  "Test if Accept-Charset header exists"
   [resource request response state]
   (if (header-value "accept-charset" (:headers request))
     #(e6 resource request response (assoc state :e5 true))
     #(f6 resource request response (assoc state :e5 false))))
 
 (defn d5
+  "Test if acceptable language is available"
   [resource request response state]
   (let [acceptable (acceptable-type
                     (let [languages (apply-callback request resource
@@ -551,12 +591,14 @@
       (response-code 406 request response state :d5))))
 
 (defn d4
+  "Test if Accept-Language header exists"
   [resource request response state]
   (if (header-value "accept-language" (:headers request))
     #(d5 resource request response (assoc state :d4 true))
     #(e5 resource request response (assoc state :d4 false))))
 
 (defn c4
+  "Test if acceptable media type is available"
   [resource request response state]
   (let [acceptable (acceptable-content-type
                     resource (header-value "accept" (:headers request)))]
@@ -568,12 +610,14 @@
       (response-code 406 request response state :c4))))
 
 (defn c3
+  "Test if Accept header exists"
   [resource request response state]
   (if (header-value "accept" (:headers request))
     #(c4 resource request response (assoc state :c3 false))
     #(d4 resource request response (assoc state :c3 true))))
 
 (defn b3
+  "Test if OPTIONS header exists"
   [resource request response state]
   (if (= :options (:request-method request))
     (response-ok request
@@ -584,6 +628,7 @@
     #(c3 resource request response (assoc state :b3 false))))
 
 (defn b4
+  "Test if request entity is too large"
   [resource request response state]
   (decide #(apply-callback request resource :valid-entity-length?)
           true
@@ -591,6 +636,7 @@
           (response-code 413 request response state :b4)))
 
 (defn b5
+  "Test if the request content type is a known content type"
   [resource request response state]
   (decide #(apply-callback request resource :known-content-type?)
           true
@@ -598,6 +644,7 @@
           (response-code 415 request response state :b5)))
 
 (defn b6
+  "Test if the Content-* headers are valide"
   [resource request response state]
   (decide #(apply-callback request resource :valid-content-headers?)
           true
@@ -605,6 +652,7 @@
           (response-code 501 request response state :b6)))
 
 (defn b7
+  "Test if this resource is forbidden"
   [resource request response state]
   (decide #(apply-callback request resource :forbidden?)
           true
@@ -612,6 +660,7 @@
           #(b6 resource request response (assoc state :b6 true))))
 
 (defn b8
+  "Test if this request is authorized to access this resource"
   [resource request response state]
   (let [result (#(apply-callback request resource :is-authorized?))]
     (cond
@@ -630,6 +679,7 @@
       (response-code 401 request response state :b8))))
 
 (defn b9b
+  "Test if this request is Malformed"
   [resource request response state]
   (decide #(apply-callback request resource :malformed-request?)
           true
@@ -637,6 +687,7 @@
           #(b8 resource request response (assoc state :b9b false))))
 
 (defn b9a
+  "Test if the Content-MD5 is valid"
   [resource request response state]
   (let [valid (apply-callback request resource :validate-content-checksum)]
 
@@ -666,6 +717,7 @@
                       state :b9a))))
 
 (defn b9
+  "Test if the Content-MD5 header exists"
   [resource request response state]
   (decide #(some (fn [[head]]
                    (= "content-md5" head))
@@ -675,6 +727,7 @@
           #(b9b resource request response (assoc state :b9 false))))
 
 (defn b10
+  "Test if the request method is allowed"
   [resource request response state]
   (decide #(some (fn [method-in]
                    (= (:request-method request) method-in))
@@ -689,6 +742,7 @@
            state :b10)))
 
 (defn b11
+  "Test if the request URI is too long"
   [resource request response state]
   (decide #(apply-callback request resource :uri-too-long?)
           true
@@ -696,6 +750,7 @@
           #(b10 resource request response (assoc state :b11 false))))
 
 (defn b12
+  "Test if the request method is a known method"
   [resource request response state]
   (decide #(some (fn [method-in]
                    (= (:request-method request) method-in))
