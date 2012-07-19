a4b27e84d7f362edfb06d5ec3275cc1d71a87f10
Christopher Miles <twitch@nervestaple.com>
2012-07-19 09:43:47 -0400

    Incremented the version number for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tnrglobal/bishop "1.1.1"
+(defproject tnrglobal/bishop "1.1.2"
   :description "A Webmachine-like REST library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
