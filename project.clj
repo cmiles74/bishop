898dfc561b9b906352bc094fe7132722cd7649e0
Christopher M. Miles <twitch@nervestaple.com>
2012-09-12 11:13:41 -0400

    Updated to use Ring 1.1.5. Moved Clojure dependency to the :dev profile.
    I also incremented the version number.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
-(defproject tnrglobal/bishop "1.1.6"
+(defproject tnrglobal/bishop "1.1.7"
   :description "A Webmachine-like REST library"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [ring/ring "1.0.1"]
+  :dependencies [[org.clojure/tools.logging "0.2.3"]
+                 [ring/ring "1.1.5"]
                  [joda-time "2.1"]
                  [commons-codec "1.5"]
                  [commons-lang "2.6"]]
-  :dev-dependencies [[ring/ring-devel "1.0.1"]
+  :dev-dependencies [[org.clojure/clojure "1.3.0"]
+                     [ring/ring-devel "1.1.5"]
                      [net.cgrand/moustache "1.1.0"
                       :exclusions [org.clojure/clojure]]]
   :profiles {:dev
-             {:dependencies [[ring/ring-devel "1.0.1"]
+             {:dependencies [[org.clojure/clojure "1.3.0"]
+                             [ring/ring-devel "1.1.5"]
                              [net.cgrand/moustache "1.1.0"
                               :exclusions [org.clojure/clojure]]]
               :plugins [[lein-swank "1.4.4"]]}})
