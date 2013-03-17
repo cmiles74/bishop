5371d0ccdd862f49b3580d165ff26ae7b410f1b3
Christopher Miles <miles@Sophie.(none)>
2013-03-17 11:41:12 -0400

    Updated to Clojure 1.5.1 and Ring 1.1.8. Updated the Travis CI configuration.

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject tnrglobal/bishop "1.2.0"
   :description "A Webmachine-like REST library"
   :dependencies [[org.clojure/tools.logging "0.2.3"]
-                 [ring/ring "1.1.5"]
+                 [ring/ring "1.1.8"]
                  [joda-time "2.1"]
                  [commons-codec "1.5"]
                  [commons-lang "2.6"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]
-                     [ring/ring-devel "1.1.5"]
+                     [ring/ring-devel "1.1.8"]
                      [net.cgrand/moustache "1.1.0"
                       :exclusions [org.clojure/clojure]]]
   :profiles {:dev
-             {:dependencies [[org.clojure/clojure "1.3.0"]
-                             [ring/ring-devel "1.1.5"]
+             {:dependencies [[org.clojure/clojure "1.5.1"]
+                             [ring/ring-devel "1.1.8"]
                              [net.cgrand/moustache "1.1.0"
                               :exclusions [org.clojure/clojure]]]
-              :plugins [[lein-swank "1.4.4"]]}})
+              :plugins [[lein-swank "1.4.5"]]}})
